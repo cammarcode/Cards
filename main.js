4a9b63961c38851b463fcfd75c3db4ddef4de140
cammarcode <nzcamm@gmail.com>
2023-12-17 21:20:09 +1300

    moving computers

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var x
 var y
 var down
+var downnum = -1
 var cardnum = 0
 
 addEventListener('mousemove', tellPos, false);
@@ -10,21 +11,22 @@ addEventListener('mouseup', release);
 window.onload = start;
 
 function tellPos(p){
-    info.innerHTML = 'Position X : ' + p.pageX + '<br />Position Y : ' + document.getElementById("testcard").style.top;
     x = p.pageX
     y = p.pageY
+
     if(down == true){
-        document.getElementById("testcard").style.left = String(x-25)+"px";
-        document.getElementById("testcard").style.top = String(y-35)+"px";
+        document.getElementById(String(downnum)).style.left = String(x-25)+"px";
+        document.getElementById(String(downnum)).style.top = String(y-35)+"px";
+
     }
     
   }
 
 function click(){
-    info.innerHTML = info.innerHTML + '<br />clicking';
-    console.log(parseInt(document.getElementById("testcard").style.left,10));
+    for(i=0; i<cardnum; i++)
     if(x>parseInt(document.getElementById("testcard").style.left) && x < parseInt(document.getElementById("testcard").style.left) + 50 && y>parseInt(document.getElementById("testcard").style.top) && y < parseInt(document.getElementById("testcard").style.top) + 70){
     down = true
+    downnum = i
     console.log("hit")
     }
     else{
@@ -33,9 +35,11 @@ function click(){
             var card = document.createElement('div');
              card.id = String(cardnum);
              card.class = "card";
+             card.style.left = "0px"
+             card.style.top = "0px"
              var cardimg = document.createElement("img");
              cardimg.src = "testcard2.png";     
-            document.body.appendChild(card);
+            document.getElementById("maindiv").appendChild(card);
             document.getElementById(String(cardnum)).appendChild(cardimg);
             cardnum +=1;
             console.log('did the thing')
@@ -49,8 +53,6 @@ function release(){
 }
 
 function start(){
-    document.getElementById("testcard").style.left = String(0)+"px";
-    document.getElementById("testcard").style.top = String(0)+"px";
     document.getElementById("deck").style.left = String(500)+"px";
     document.getElementById("deck").style.top = String(500)+"px";
 }
