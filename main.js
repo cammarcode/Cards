1db9280e4354259fc4f38d4c7a859cd7931228ec
cammarcode <nzcamm@gmail.com>
2023-12-17 17:52:26 +1300

    Deck makes cards!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var x
 var y
 var down
-
+var cardnum = 0
 
 addEventListener('mousemove', tellPos, false);
 addEventListener('mousedown', click);
@@ -29,6 +29,16 @@ function click(){
     }
     else{
         if(x>parseInt(document.getElementById("deck").style.left) && x < parseInt(document.getElementById("deck").style.left) + 50 && y>parseInt(document.getElementById("deck").style.top) && y < parseInt(document.getElementById("deck").style.top) + 70){
+            console.log('doing the thing')
+            var card = document.createElement('div');
+             card.id = String(cardnum);
+             card.class = "card";
+             var cardimg = document.createElement("img");
+             cardimg.src = "testcard2.png";     
+            document.body.appendChild(card);
+            document.getElementById(String(cardnum)).appendChild(cardimg);
+            cardnum +=1;
+            console.log('did the thing')
 
         }
     }
@@ -41,4 +51,6 @@ function release(){
 function start(){
     document.getElementById("testcard").style.left = String(0)+"px";
     document.getElementById("testcard").style.top = String(0)+"px";
+    document.getElementById("deck").style.left = String(500)+"px";
+    document.getElementById("deck").style.top = String(500)+"px";
 }
